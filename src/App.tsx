606bca6ffbf38914886dc7ad4d3e044b02cfec22
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-12-01 16:50:35 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,19 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <header></header>
-      <main></main>
+    <div className="app">
+      <header>
+        <h1>Test React</h1>
+      </header>
+      <main>
+        <h2>lol xd</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, ex ab,
+          soluta quisquam molestiae nihil illo, distinctio nam laboriosam itaque
+          quas laudantium similique suscipit! Vero incidunt deleniti sunt
+          aliquid suscipit?
+        </p>
+      </main>
     </div>
   )
 }
